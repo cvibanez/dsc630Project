2104f1588e5fea494dc8b8b46afbfd162b5bb08e
cvibanez <48417203+cvibanez@users.noreply.github.com>
2020-09-10 21:04:09 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Conrad Ibanez
+# DSC540 Spring 2020
+# Weeks 3 and 4 Exercises
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def print_function():
+    
+    print('in print function')
+    
+    
+
+def main():
+    
+    print_function()
+ 
+if __name__ == '__main__':
+    main()
+    
